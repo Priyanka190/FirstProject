b82eafe8eb47325cd1beb9affa206cd83db57517
Priyanka190 <parikumarisingh28@gmail.com>
2021-05-11 22:30:39 +0530

    InsertionInSinglyLinkedList

--- src/com/priyanka/DataStructure/Run.java
+++ src/com/priyanka/DataStructure/Run.java
@@ -1,9 +1,10 @@
 package com.priyanka.DataStructure;
 
 import java.util.Scanner;
+import java.lang.Exception;
 
 public class Run {
-    public static void main(String[] agrs) {
+    public static void main(String[] ars) throws Exception{
         Scanner sc = new Scanner(System.in);
         SinglyLinkedList list = new SinglyLinkedList();
         System.out.println("enter the num of node:\n");
@@ -13,5 +14,30 @@ public class Run {
             list.add(data);
         }
         System.out.println(list);
+        int operation;
+        System.out.println("enter the desired operation number:");
+        operation=sc.nextInt();
+        switch(operation) {
+            case 1:System.out.println("insert at the starting:\n");
+                System.out.println("enter data:\n");
+                int data=sc.nextInt();
+                list.insertAtStart(data);
+                System.out.println(list);
+                break;
+            case 2:System.out.println("insert at given position:\n");
+                System.out.println("enter the position:\n");
+                int pos;
+                pos=sc.nextInt();
+                System.out.println("enter data:\n");
+                data=sc.nextInt();
+                list.insertAtMiddle(data,pos);
+                System.out.println(list);
+                break;
+            case 3:System.out.println("insert at the end of list:\n" );
+                System.out.println("enter data:\n");
+                data=sc.nextInt();
+                list.insertAtEnd(data);
+                System.out.println(list);
+        }
     }
 }
--- src/com/priyanka/DataStructure/SinglyLinkedList.java
+++ src/com/priyanka/DataStructure/SinglyLinkedList.java
@@ -2,7 +2,7 @@ package com.priyanka.DataStructure;
 
 import java.util.Scanner;
 
-public class SinglyLinkedList {
+class SinglyLinkedList {
     Node head;
 
     public SinglyLinkedList() {
@@ -21,6 +21,47 @@ public class SinglyLinkedList {
 
         }
     }
+
+    public void insertAtStart(int data) {
+        if (head == null) {
+            head = new Node(data);
+        } else {
+            Node temp=new Node(data);
+            temp.next=head;
+            head=temp;
+        }
+
+    }
+    public void insertAtMiddle(int data, int pos){
+        if(head==null) {
+            System.out.println("no operation is done:\n");
+        }
+        else {
+            Node n=head;
+            int i=0;
+            while(i<pos-1 && n!=null) {
+                n=n.next;
+                i++;
+            }
+            Node temp=new Node(data);
+            temp.next=n.next;
+            n.next=temp;
+        }
+    }
+    public void insertAtEnd(int data) {
+        if(head==null) {
+            System.out.println("no operation is done:\n");
+        }
+        else {
+            Node n=head;
+            while(n.next!=null) {
+                n=n.next;
+            }
+            n.next=new Node(data);
+        }
+    }
+
+
 
     public String toString() {
         return "" + head;
