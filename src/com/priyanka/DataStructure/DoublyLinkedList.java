16ce5625ceba469b4bed891af6f6b22c7b2ed495
Priyanka190 <parikumarisingh28@gmail.com>
2021-05-14 16:27:17 +0530

    insertion DoublyLinkedList

--- src/com/priyanka/DataStructure/DoublyLinkedList.java
+++ src/com/priyanka/DataStructure/DoublyLinkedList.java
@@ -22,6 +22,51 @@ public class DoublyLinkedList {
             n.next=temp;
         }
     }
+    public void insertAtStart(int data) {
+        if(head==null) {
+            head=new Node(data);
+        }
+        else {
+            Node temp=new Node(data);
+            temp.next=head;
+            head=temp;
+        }
+    }
+    public void insertAtMid(int data,int pos) {
+        if(head==null) {
+            System.out.println("nothing to be performed");
+        }
+        else {
+           Node n=head;
+           int i=0;
+            while(i<pos-1 && n.next!=null) {
+                n=n.next;
+                i++;
+            }
+            Node temp=new Node(data);
+            temp.next=n.next;
+            temp.prev=n;
+            if(n.next!=null) {
+                n.next.prev=temp;
+                n.next=temp;
+            }
+        }
+    }
+    public void insertAtEnd(int data) {
+        if(head==null) {
+            System.out.println("nothing can be done");
+        }
+        else {
+            Node n=head;
+            while(n.next!=null) {
+                n=n.next;
+            }
+            Node temp=new Node(data);
+            temp.next=null;
+            temp.prev=n;
+            n.next=temp;
+        }
+    }
     public String toString() {
         return ""+head;
     }
--- src/com/priyanka/DataStructure/Run.java
+++ src/com/priyanka/DataStructure/Run.java
@@ -14,6 +14,31 @@ public class Run {
             list.add(data);
         }
         System.out.println(list);
+        int operation;
+        System.out.println("enter the desired operation number:");
+        operation=sc.nextInt();
+        switch(operation) {
+            case 1:System.out.println("insert at the starting:\n");
+                System.out.println("enter data:\n");
+                int data=sc.nextInt();
+                list.insertAtStart(data);
+                System.out.println(list);
+                break;
+            case 2:System.out.println("insert at given position:\n");
+                System.out.println("enter the position:\n");
+                int pos;
+                pos=sc.nextInt();
+                System.out.println("enter data:\n");
+                data=sc.nextInt();
+                list.insertAtMid(data,pos);
+                System.out.println(list);
+                break;
+            case 3:System.out.println("insert at the end of list:\n" );
+                System.out.println("enter data:\n");
+                data=sc.nextInt();
+                list.insertAtEnd(data);
+                System.out.println(list);
+        }
     }
 }
 
