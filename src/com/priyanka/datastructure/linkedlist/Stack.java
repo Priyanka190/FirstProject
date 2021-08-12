60f72a200d684a162cbb8e41124e336986b3537e
Priyanka190 <parikumarisingh28@gmail.com>
2021-08-12 21:41:57 +0530

    Balanced parathsis

--- src/com/priyanka/datastructure/linkedlist/Stack.java
+++ src/com/priyanka/datastructure/linkedlist/Stack.java
@@ -1,25 +1,23 @@
 package com.priyanka.datastructure.linkedlist;
 
 public class Stack {
-    int data[];
+    char data[];
     int capacity;
     int top1;
-    int top2;
     public Stack(int capacity) {
         this.capacity=capacity;
         this.top1=-1;
-        this.top2=capacity;
-        data=new int[capacity];
+        data= new char[capacity];
         for(int i=0; i<capacity; i++) {
-            data[i]=-1;
+            data[i]='0';
         }
     }
 
-    public int pop1() {
-        int val;
+    public char pop1() {
+        char val;
         if (top1 == -1) {
             System.out.println("STACK IS UNDERFLOW ");
-            return -1;
+            return '0';
         } else {
             val = data[top1];
             top1--;
@@ -27,20 +25,8 @@ public class Stack {
         }
     }
 
-    public int pop2() {
-        int val;
-        if (top2 == capacity) {
-            System.out.println("STACK IS UNDERFLOW ");
-            return -1;
-        } else {
-            val = data[top2];
-            top2++;
-            return val;
-        }
-    }
-
-    public void push1(int val) {
-        if (top1 == top2 - 1) {
+    public void push1(char val) {
+        if (top1 == capacity-1) {
             System.out.println("STACK IS OVERFLOW");
         } else {
             top1++;
@@ -48,29 +34,20 @@ public class Stack {
         }
     }
 
-    public void push2(int val) {
-        if (top2 - 1 == top1) {
-            System.out.println("STACK IS OVERFLOW");
-        } else {
-            top2--;
-            data[top2] = val;
-        }
-    }
-
-    public int peek1() {
+    public char peek1() {
         if (top1 == -1) {
             System.out.println("underflow data");
-            return -1;
+            return '1';
         }
         return data[top1];
     }
-
-    public int peek2() {
-        if (top1 == capacity) {
-            System.out.println("underflow data");
-            return -1;
+    public boolean isEmpty() {
+        if(top1==-1)   {
+            return true;
         }
-        return data[top1];
+        else {
+            return  false;
+        }
     }
 
     public void display1() {
@@ -78,13 +55,6 @@ public class Stack {
             System.out.println(data[i]);
         }
     }
-
-    public void display2() {
-        for (int i = top2; i < capacity; i++) {
-            System.out.println(data[i]);
-        }
-    }
-
     public void display() {
         for (int i = 0; i < capacity; i++) {
             System.out.print(data[i] + " ");
--- src/com/priyanka/implementation/Run.java
+++ src/com/priyanka/implementation/Run.java
@@ -1,18 +1,42 @@
 package com.priyanka.implementation;
-
+import com.priyanka.datastructure.linkedlist.Stack;
 import javax.swing.plaf.metal.MetalBorders;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+
 public class Run {
     public static void main(String[] agrs) throws FileNotFoundException,Exception {
         System.setIn(new FileInputStream("C:\\Users\\KIIT\\IdeaProjects\\FirstProject\\src\\interaction\\input.txt"));
         System.setOut(new PrintStream(new FileOutputStream("C:\\Users\\KIIT\\IdeaProjects\\FirstProject\\src\\interaction\\output.txt")));
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        palindromic p= new palindromic();
-        p.palindrom(input);
+        String s;
+        s = sc.next();
+        Stack stack = new Stack(s.length());
+        char element;
+        for(int i=0; i<s.length(); i++) {
+            element = s.charAt(i);
+            if(element=='(') {
+                stack.push1(element);
+            }
+            else {
+                if(stack.peek1()=='(') {
+                    int popped=stack.pop1();
+                }
+                else {
+                    stack.push1(element);
+                    System.out.println("unbalanced");
+                }
+            }
+        }
+        if(stack.isEmpty()==true) {
+            System.out.println("balanced");
+        }
+        else {
+            System.out.println("not");
+        }
+        stack.display1();
     }
 }
