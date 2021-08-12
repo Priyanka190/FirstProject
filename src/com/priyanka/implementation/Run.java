5efb0929a2224c69e827807fdcedefb089e4ab41
Priyanka190 <parikumarisingh28@gmail.com>
2021-08-12 23:51:37 +0530

    Balanced brackets using collections

--- src/com/priyanka/implementation/Run.java
+++ src/com/priyanka/implementation/Run.java
@@ -1,13 +1,12 @@
 package com.priyanka.implementation;
 
-import com.priyanka.datastructure.linkedlist.Stack;
 
-import javax.swing.plaf.metal.MetalBorders;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Run {
     public static void main(String[] agrs) throws FileNotFoundException, Exception {
@@ -16,42 +15,38 @@ public class Run {
         Scanner sc = new Scanner(System.in);
         String s;
         s = sc.next();
-        Stack stack = new Stack(s.length());
+        Stack<Character> stack = new Stack();
         char element;
         boolean isValid=true;
         for (int i = 0; i < s.length(); i++) {
             element = s.charAt(i);
             if (element == '(' || element == '{' || element == '[') {
-                stack.push1(element);
+                stack.push(element);
             } else if (element == ')') {
-                if (stack.peek1() == '(') {
-                    stack.pop1();
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
                 }
                 else {
-                    System.out.println("unbalanced");
                     isValid=false;
                     break;
                 }
             } else if (element == '}') {
-                if (stack.peek1() == '{') {
-                    stack.pop1();
+                if (!stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop();
                 }
                 else {
-                    System.out.println("unbalanced");
                     isValid=false;
                     break;
                 }
             } else if (element == ']') {
-                if (stack.peek1() == '[') {
-                    stack.pop1();
+                if (!stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
                 }
                 else{
-                    System.out.println("unbalanced");
                     isValid=false;
                     break;
                 }
             } else {
-                System.out.println("unbalanced");
                 isValid=false;
             }
         }
@@ -60,6 +55,5 @@ public class Run {
         }else {
             System.out.println("invalid string");
         }
-        stack.display1();
     }
 }
