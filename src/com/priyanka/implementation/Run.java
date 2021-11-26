9ecdcc1484d974ca7484fb97e1021392289ac775
Priyanka190 <parikumarisingh28@gmail.com>
2021-11-26 16:16:23 +0530

    ATM problem

--- src/com/priyanka/implementation/Run.java
+++ src/com/priyanka/implementation/Run.java
@@ -12,39 +12,12 @@ public class Run {
         System.setIn(new FileInputStream("C:\\Users\\KIIT\\IdeaProjects\\FirstProject\\src\\interaction\\input.txt"));
         System.setOut(new PrintStream(new FileOutputStream("C:\\Users\\KIIT\\IdeaProjects\\FirstProject\\src\\interaction\\output.txt")));
         Scanner sc = new Scanner(System.in);
-        int size1=sc.nextInt();
-        int size2=sc.nextInt();
-        List<Integer> list1 = new LinkedList<Integer>();
-        List<Integer> list2 = new LinkedList<Integer>();
-        for(int i=0; i<size1; i++) {
-            int temp=sc.nextInt();
-            list1.add(temp);
-        }
-        for(int j=0; j<size2; j++) {
-            int temp1=sc.nextInt();
-            list2.add(temp1);
-        }
-        int diff=size1-size2;
-        if(diff<0) {
-            diff=-diff;
-        }
-        if(diff!=0) {
-            if(size1>size2) {
-                for(int k=0; k<diff; k++) {
-                    int element=0;
-                    list2.add(element);
-                }
-            }
-            else{
-                for(int p=0; p<diff; p++) {
-                    int element=0;
-                    list1.add(element);
-                }
-            }
-        }
         twoSum l  = new twoSum();
-        List<Integer> result =new LinkedList<>();
-        result=l.twoSum1(list1,list2);
+        float balance = sc.nextFloat();
+        int amount = sc.nextInt();
+        float totalBalnce=0;
+        totalBalnce=l.atm(amount,balance);
+        System.out.println(totalBalnce);
     }
 
 }
--- src/com/priyanka/implementation/twoSum.java
+++ src/com/priyanka/implementation/twoSum.java
@@ -6,31 +6,16 @@ import java.util.*;
 
 public class twoSum {
 
-    public List<Integer> twoSum1(List list1,List list2) {
-        List<Integer> finslList = new LinkedList<>();
-        int sum=0,qouteint=0,rem=0;
-        int temp1=0,temp2 = 0;
-        for(int i=0; i<list1.size(); i++) {
-            temp1= (int) list1.get(i);
-            temp2= (int) list2.get(i);
-            sum=temp1+temp2+qouteint;
-            if(sum<=9) {
-                qouteint=0;
-                finslList.add(sum);
-            }
-            else {
-                qouteint=0;
-                rem=sum%10;
-                qouteint=sum/10;
-                finslList.add(rem);
-            }
-        }
-        if(qouteint!=0) {
-            finslList.add(qouteint);
-        }
-        System.out.println(finslList);
-        return finslList;
-    }
-
+   public float atm(int amount,float balance) {
+       float totalAmount=0;
+       if(amount%5==0 && amount<=balance) {
+           totalAmount=balance-amount;
+           totalAmount= (float) (totalAmount-0.5);
+       }
+       else {
+           return balance;
+       }
+       return totalAmount;
+   }
 
 }
