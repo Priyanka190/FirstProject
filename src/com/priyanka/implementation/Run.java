035d58be84588777696ebc6c3591df5037d96c33
Priyanka190 <parikumarisingh28@gmail.com>
2021-12-22 21:08:11 +0530

    fourSum

--- src/com/priyanka/implementation/Run.java
+++ src/com/priyanka/implementation/Run.java
@@ -3,21 +3,25 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Run {
+
     public static void main(String[] agrs) throws FileNotFoundException,Exception {
         System.setIn(new FileInputStream("C:\\Users\\KIIT\\IdeaProjects\\FirstProject\\src\\interaction\\input.txt"));
         System.setOut(new PrintStream(new FileOutputStream("C:\\Users\\KIIT\\IdeaProjects\\FirstProject\\src\\interaction\\output.txt")));
         Scanner sc = new Scanner(System.in);
-        twoSum l  = new twoSum();
-        float balance = sc.nextFloat();
-        int amount = sc.nextInt();
-        float totalBalnce=0;
-        totalBalnce=l.atm(amount,balance);
-        System.out.println(totalBalnce);
+        int size=sc.nextInt();
+        int target=sc.nextInt();
+        int[] a= new int[size];
+        for(int i=0; i<size; i++) {
+            a[i]=sc.nextInt();
+        }
+        fourSum f=new fourSum();
+        List<List<Integer>> result =f.fourSum(a,target);
+        System.out.println(result);
     }
 
 }
